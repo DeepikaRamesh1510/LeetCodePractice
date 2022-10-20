e73570bf571ec3feed1e2d79bd9803085ac8cd3e
Deepika <deepikardb@gmail.com>
2022-10-20 12:57:55 -0700

    Easy problem -1

---
+++ TwoSum.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+/*
+ 1. Two Sum
+ 
+ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ You can return the answer in any order.
+
+  
+
+ Example 1:
+
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ Example 2:
+
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+ */
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var numDict: [Int: Int] = [:]
+    for (firstIndex, firstNumber) in nums.enumerated() {
+        let secondNumber = target - firstNumber
+        if let secondIndex = numDict[secondNumber] {
+            return [firstIndex, secondIndex]
+        }
+        numDict[firstNumber] = firstIndex
+    }
+    return [-1]
+}
