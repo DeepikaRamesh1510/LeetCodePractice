75f6625f4d7754976d3350f6a5864b5f37932762
Deepika <deepikardb@gmail.com>
2023-02-28 17:16:07 -0800

    344. Reverse String - https://leetcode.com/problems/reverse-string

---
+++ 344_ReverseString.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+/*
+ 344. Reverse String - https://leetcode.com/problems/reverse-string
+ 
+ Write a function that reverses a string. The input string is given as an array of characters s.
+ 
+ You must do this by modifying the input array in-place with O(1) extra memory.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: s = ["h","e","l","l","o"]
+ Output: ["o","l","l","e","h"]
+ Example 2:
+ 
+ Input: s = ["H","a","n","n","a","h"]
+ Output: ["h","a","n","n","a","H"]
+ 
+ 
+ Constraints:
+ 
+ 1 <= s.length <= 105
+ s[i] is a printable ascii character.
+ */
+
+
+func reverseString(_ s: inout [Character]) {
+	var left = 0
+	var right = s.count - 1
+	
+	while left <= right {
+		let temp = s[left]
+		s[left] = s[right]
+		s[right] = temp
+		left += 1
+		right -= 1
+	}
+}
+
+//var s: [Character] = ["h","e","l","l","o"]
+//
+//reverseString(&s)
+//
+//print(s)
