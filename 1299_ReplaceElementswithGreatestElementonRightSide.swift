84035b7a3f0d4c0041fa7fa66f7bb85a2c6c9f8f
Deepika <deepikardb@gmail.com>
2023-05-05 10:35:18 -0700

    1299. Replace Elements with Greatest Element on Right Side - https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/

---
+++ 1299_ReplaceElementswithGreatestElementonRightSide.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+/*
+ 1299. Replace Elements with Greatest Element on Right Side - https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+ Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+ 
+ After doing so, return the array.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: arr = [17,18,5,4,6,1]
+ Output: [18,6,6,6,1,-1]
+ Explanation:
+ - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+ - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+ - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+ - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+ - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+ - index 5 --> there are no elements to the right of index 5, so we put -1.
+ Example 2:
+ 
+ Input: arr = [400]
+ Output: [-1]
+ Explanation: There are no elements to the right of index 0.
+ 
+ 
+ Constraints:
+ 
+ 1 <= arr.length <= 104
+ 1 <= arr[i] <= 105
+ */
+
+
+func replaceElements(_ arr: [Int]) -> [Int] {
+	guard arr.count > 1 else { return [-1] }
+	var nums = arr
+	var maxValue = -1
+	
+	for i in stride(from: nums.count - 1, to: -1, by: -1) {
+		let current = nums[i]
+		nums[i] = maxValue
+		maxValue = max(current, maxValue)
+	}
+	
+	return nums
+	
+}
